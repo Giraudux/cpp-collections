ebdffb5d26a5951e02f61e43a329e6921f3d7dc0
Giraudux <Giraudux@users.noreply.github.com>
2014-02-14 11:41:45 +0100

    add multi_set

---
+++ src/multi_set.hpp
@@ -0,0 +1,30 @@
+// Alexis Giraudet
+// Théo Cesbron
+
+#ifndef MULTI_SET_HPP
+#define MULTI_SET_HPP
+
+#include <exception>
+
+template <typename T>
+class multi_set
+{
+    public:
+        multi_set();
+        ~multi_set();
+};
+
+template <typename T>
+multi_set<T>::multi_set()
+{
+    ;
+}
+
+template <typename T>
+multi_set<T>::~multi_set()
+{
+    
+    ;
+}
+
+#endif
