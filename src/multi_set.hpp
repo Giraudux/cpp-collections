dac0264aabf82208ced9bf6f076abb2b7a628ac2
Giraudux <giraudux@gmail.com>
2014-03-08 17:25:16 +0100

    speed commit

--- src/multi_set.hpp
+++ src/multi_set.hpp
@@ -10,13 +10,15 @@ class multi_set
     public:
         virtual void add(T type) = 0;
         virtual void clear() = 0;
-        virtual bool equal(const multi_set &mul) = 0;   
-        virtual void fusion(const multi_set &mul) = 0;
-        virtual void intersection(const multi_set &mul) = 0;
+        virtual int count(T type) = 0;
+        virtual bool equal(const multi_set &ms) = 0;   
+        virtual void fusion(const multi_set &ms) = 0;
+        virtual void intersection(const multi_set &ms) = 0;
         virtual bool is_empty() = 0;
         virtual void remove(T type) = 0;
         virtual void remove_all(T type) = 0;
-        virtual void sub(multi_set &mul) = 0;
+        virtual int size() = 0;
+        virtual void sub(multi_set &ms) = 0;
 };
 
 #endif
