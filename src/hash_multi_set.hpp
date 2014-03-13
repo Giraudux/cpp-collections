f6f301caacbff39794243862ed01a4f8586266be
Giraudux <giraudux@gmail.com>
2014-03-13 17:51:28 +0100

    add link_multi_set and hash_multi_set

---
+++ src/hash_multi_set.hpp
@@ -0,0 +1,212 @@
+// Alexis Giraudet
+// Théo Cesbron
+
+#ifndef HASH_MULTI_SET_HPP
+#define HASH_MULTI_SET_HPP
+
+#include "hash_table.hpp"
+
+template <typename T, int S>
+class hash_multi_set
+{
+    private:
+        hash_table<T,int,S> _hash_table;
+
+    public:
+        hash_multi_set();
+        ~hash_multi_set();
+        void add(T type);
+        void clear();
+        int count(T type);
+        bool equal(const hash_multi_set &hms);   
+        void fusion(const hash_multi_set &hms);
+        void intersection(const hash_multi_set &hms);
+        bool is_empty();
+        void remove(T type);
+        void remove_all(T type);
+        int size();
+        void sub(hash_multi_set &hms);
+};
+
+template <typename T, int S>
+hash_multi_set<T,S>::hash_multi_set()
+{
+    ;
+}
+
+template <typename T, int S>
+hash_multi_set<T,S>::~hash_multi_set()
+{
+    ;
+}
+
+template <typename T, int S>
+void hash_multi_set<T,S>::add(T type)
+{
+    try
+    {
+        int i = _hash_table.get(type);
+        _hash_table.put(type, i+1);
+    }
+    catch(std::exception &e)
+    {
+        _hash_table.put(type, 1);
+    }
+}
+
+template <typename T, int S>
+void hash_multi_set<T,S>::clear()
+{
+    _hash_table.clear();
+}
+
+template <typename T, int S>
+int hash_multi_set<T,S>::count(T type)
+{
+    try
+    {
+        int i = _hash_table.get(type);
+        return i;
+    }
+    catch(std::exception &e)
+    {
+        return 0;
+    }
+}
+
+template <typename T, int S>
+bool hash_multi_set<T,S>::equal(const hash_multi_set &hms)
+{
+    if(size() != hms.size())
+    {
+        return false;
+    }
+    else
+    {
+        bool res = true;
+        T *tmp = _hash_table.keys_array();
+        for(int i=0; i<_hash_table.size(); i++)
+        {
+            if(count(tmp[i]) != hms.count(tmp[i]))
+            {
+                res = false;
+                break;
+            }
+        }
+        delete[] tmp;
+        return res;
+    }
+}
+
+template <typename T, int S>
+void hash_multi_set<T,S>::fusion(const hash_multi_set &hms)
+{
+    T *tmp = hms._hash_table.keys_array();
+    for(int i=0; i<hms._hash_table.size(); i++)
+    {
+        try
+        {
+            int j = _hash_table.get(tmp[i]);
+            _hash_table.put(tmp[i], j+hms._hash_table.get(tmp[i]));
+        }
+        catch(std::exception &e)
+        {
+            _hash_table.put(tmp[i], hms._hash_table.get(tmp[i]));
+        }
+    }
+    delete[] tmp;
+}
+
+template <typename T, int S>
+void hash_multi_set<T,S>::intersection(const hash_multi_set &hms)
+{
+    T *tmp = _hash_table.keys_array();
+    for(int i=0; i<_hash_table.size(); i++)
+    {
+        try
+        {
+            int j = hms._hash_table.get(tmp[i]);
+            int k = _hash_table.get(tmp[i]);
+            if(j<k)
+            {
+                _hash_table.put(tmp[i], j);
+            }
+        }
+        catch(std::exception &e)
+        {
+            _hash_table.remove_all(tmp[i]);
+        }
+    }
+    delete[] tmp;
+}
+
+template <typename T, int S>
+bool hash_multi_set<T,S>::is_empty()
+{
+    return _hash_table.is_empty();
+}
+
+template <typename T, int S>
+void hash_multi_set<T,S>::remove(T type)
+{
+    try
+    {
+        int i = _hash_table.get(type);
+        if(i>1)
+        {
+            _hash_table.put(type, i-1);
+        }
+        else
+        {
+            _hash_table.remove(type);
+        }
+    }
+    catch(std::exception &e)
+    {
+        ;
+    }
+}
+
+template <typename T, int S>
+void hash_multi_set<T,S>::remove_all(T type)
+{
+    _hash_table.remove(type);
+}
+
+template <typename T, int S>
+int hash_multi_set<T,S>::size()
+{
+    int res=0;
+    int *tmp = _hash_table.values_array();
+    for(int i=0; i<_hash_table.size(); i++)
+    {
+        res += tmp[i];
+    }
+    delete[] tmp;
+    return res;
+}
+
+template <typename T, int S>
+void hash_multi_set<T,S>::sub(hash_multi_set &hms)
+{
+    T *tmp = _hash_table.keys_array();
+    for(int i=0; i<_hash_table.size(); i++)
+    {
+        int j = hms.count(tmp[i]);
+        if(j != 0)
+        {
+            int k = count(tmp[i]);
+            if(k-j < 1)
+            {
+                remove_all(tmp[i]);
+            }
+            else
+            {
+                _hash_table.put(tmp[i], k-j);
+            }
+        }
+    }
+    delete[] tmp;
+}
+
+#endif
---
+++ src/link_multi_set.hpp
@@ -0,0 +1,167 @@
+// Alexis Giraudet
+// Théo Cesbron
+
+#ifndef LINK_MULTI_SET_HPP
+#define LINK_MULTI_SET_HPP
+
+template <typename T>
+struct _link
+{
+    T type;
+    _link *next;
+};
+
+template <typename T>
+class link_multi_set
+{
+    private:
+        _link<T> *_head;
+        int _size;
+
+    public:
+        link_multi_set();
+        ~link_multi_set();
+        void add(T type);
+        void clear();
+        int count(T type);
+        bool equal(const link_multi_set &lms);   
+        void fusion(const link_multi_set &lms);
+        void intersection(const link_multi_set &lms);
+        bool is_empty();
+        void remove(T type);
+        void remove_all(T type);
+        int size();
+        void sub(link_multi_set &lms);
+};
+
+template <typename T>
+link_multi_set<T>::link_multi_set(): _head(0), _size(0)
+{
+    ;
+}
+
+template <typename T>
+link_multi_set<T>::~link_multi_set()
+{
+    clear();
+}
+
+template <typename T>
+void link_multi_set<T>::add(T type)
+{
+    _link<T> *new_link = new _link<T>;
+    (*new_link).type = type;
+    (*new_link).next = _head;
+    _head = new_link;
+}
+
+template <typename T>
+void link_multi_set<T>::clear()
+{
+    _link<T> *tmp_link = _head;
+    while(tmp_link != 0)
+    {
+        _link<T> *del_link = tmp_link;
+        tmp_link = (*tmp_link).next;
+        delete del_link;
+    }
+    _head = 0;
+    _size = 0;
+}
+
+template <typename T>
+int link_multi_set<T>::count(T type)
+{
+    int res=0;
+    _link<T> *tmp_link = _head;
+    while(tmp_link != 0)
+    {
+        if((*tmp_link).type == type)
+        {
+            res++;
+        }
+        tmp_link = (*tmp_link).next;
+    }
+    return res;
+}
+
+template <typename T>
+bool link_multi_set<T>::equal(const link_multi_set &lms)//todo: optimiser
+{
+    if(size() != lms.size())
+    {
+        return false;
+    }
+    _link<T> *tmp_link = _head;
+    while(tmp_link != 0)
+    {
+        if(count((*tmp_link).type) != lms.count(*tmp_link).type)
+        {
+            return false;
+        }
+        tmp_link = (*tmp_link).next;
+    }
+    return true;
+}
+
+template <typename T>
+void link_multi_set<T>::fusion(const link_multi_set &lms)
+{
+    _link<T> *tmp_link = lms._head;
+    while(tmp_link != 0)
+    {
+        add((*tmp_link).type);
+        tmp_link = (*tmp_link).next;
+    }
+}
+
+template <typename T>
+void link_multi_set<T>::intersection(const link_multi_set &lms)
+{
+    _link<T> *tmp_link = _head;
+    while(tmp_link != 0)
+    {
+        int tmp_count = lms.count((*tmp_link).type);
+        if(tmp_count == 0)
+        {
+            remove_all((*tmp_link).type);
+        }
+        else
+        {
+            ;//ajout tmp_count type ?
+        }
+        tmp_link = (*tmp_link).next;
+    }
+}
+
+template <typename T>
+bool link_multi_set<T>::is_empty()
+{
+    return _head == 0;
+}
+
+template <typename T>
+void link_multi_set<T>::remove(T type)
+{
+    ;
+}
+
+template <typename T>
+void link_multi_set<T>::remove_all(T type)
+{
+    ;
+}
+
+template <typename T>
+int link_multi_set<T>::size()
+{
+    return _size;
+}
+
+template <typename T>
+void link_multi_set<T>::sub(link_multi_set &lms)
+{
+    ;
+}
+
+#endif
