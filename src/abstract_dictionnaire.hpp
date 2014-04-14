fb688f1654b0ec28cc31ab378a5b2f0ec268306e
Giraudux <giraudux@gmail.com>
2014-04-15 00:04:43 +0200

    fix comments

--- src/abstract_dictionnaire.hpp
+++ src/abstract_dictionnaire.hpp
@@ -17,23 +17,23 @@ class abstract_dictionnaire
 {
     public:
         /**
-         * vrai ssi la chaîne mot figure dans le hash_dictionnaire
+         * vrai ssi la chaîne mot figure dans le dictionnaire
         **/
         virtual bool contientMot(const string& mot) const = 0;
         /**
-         * ajoute la chaîne mot au hash_dictionnaire, avec la valeur v, mot étant supposé absent du hash_dictionnaire
+         * ajoute la chaîne mot au dictionnaire, avec la valeur v, mot étant supposé absent du dictionnaire
         **/
         virtual void ajouterMot(const string& mot, const V& v) = 0;
         /**
-         * associe la valeur v à la chaîne mot dans le hash_dictionnaire, mot pouvant être présent ou absent du hash_dictionnaire
+         * associe la valeur v à la chaîne mot dans le dictionnaire, mot pouvant être présent ou absent du dictionnaire
         **/
         virtual void associerMot(const string& mot, const V& v) = 0;
         /**
-         * supprime l'éventuelle chaîne mot du hash_dictionnaire
+         * supprime l'éventuelle chaîne mot du dictionnaire
         **/
         virtual void supprimerMot(const string& mot) = 0;
         /**
-         * donne la valeur correspondant à la chaîne mot (supposée figurer dans le hash_dictionnaire)
+         * donne la valeur correspondant à la chaîne mot (supposée figurer dans le dictionnaire)
         **/
         virtual V valeurAssociee(const string& mot) const = 0;
 };
